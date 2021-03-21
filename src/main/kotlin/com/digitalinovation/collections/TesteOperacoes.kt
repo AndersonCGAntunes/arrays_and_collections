720d2245305aa5d7a74956612b8b465211d4f705
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2021-03-21 10:50:20 -0300

    created array min max average filter test class

--- src/main/kotlin/com/digitalinovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/com/digitalinovation/collections/TesteOperacoes.kt
@@ -1,5 +1,7 @@
 package com.digitalinovation.collections
 
+const val ESPACADOR = "-------------------"
+
 fun main() {
     val salarios = doubleArrayOf( 1400.0, 2500.0, 4000.0 )
 
@@ -7,16 +9,30 @@ fun main() {
         println( salario )
     }
 
-    println( "||" )
+    println( ESPACADOR )
 
     println( "Maior salário: ${ salarios.maxOrNull() }" )
     println( "Menor salário: ${ salarios.minOrNull() }" )
     println( "Media salarial: ${ salarios.average() }" )
 
-    println( "||" )
+    println( ESPACADOR )
 
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
 
     salariosMaiorQue2500.forEach { println( it ) }
+
+    println( ESPACADOR )
+
+    println( salarios.count{ it in 2000.0..50000.0 } )
+
+    println( ESPACADOR )
+
+    println( salarios.find { it == 2500.0 } )
+    println( salarios.find { it == 500.0 } )
+
+    println( ESPACADOR )
+
+    println( salarios.any { it == 1400.0 } )
+    println( salarios.any { it == 500.0 } )
 
 }
